0370beca9c118cf878a879b9b7b16fcd610485e4
Светлана <svetlana@MacBook-Pro-Svetlana.local>
2022-05-30 00:35:04 +0400

    initial commit

---
+++ pdf.py
@@ -0,0 +1,14 @@
+
+from PyPDF2 import PdfReader, PdfWriter
+from sys import argv
+
+template = PdfReader(argv[1])
+watermark = PdfReader(argv[2]).pages[0]
+output = PdfWriter()
+
+for i in range(len(template.pages)):
+  page = template.pages[i]
+  page.merge_page(watermark)
+  output.add_page(page)
+  with open('watermarked_output.pdf', 'wb') as file:
+    output.write(file)
